5637cd542bb392c177b14f365cadc2804d7dbe53
Reinaldo Souza <souzarnld@gmail.com>
2024-09-05 17:33:30 -0300

    Soma dois numeros V1

---
+++ pn001-01-SomaDoisnumeros.py
@@ -0,0 +1,9 @@
+num1 = num2 = 0
+
+num1 = int(input("\nDigite o primeiro numero : "))
+
+num2 = int(input("\nDigite o segundo numero : "))
+
+soma = num1 + num2
+
+print("\nA soma de " + str(num1) + " + " + str(num2) + " é igual a : " + str(soma) + "\n")
